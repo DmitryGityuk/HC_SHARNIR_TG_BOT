c1f32c3053a28a439525167b40b5c49e452d3f3b
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-15 23:10:00 +0500

    Initial commit

---
+++ src/main/java/com/example/HC_SHARNIR_BOT/HcSharnirBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.HC_SHARNIR_BOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcSharnirBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HcSharnirBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HC_SHARNIR_BOT/HcSharnirBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HC_SHARNIR_BOT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcSharnirBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
