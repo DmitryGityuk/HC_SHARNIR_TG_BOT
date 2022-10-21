bd277785472faf3acc1b6ad93fce165d50c14aa1
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-21 23:50:58 +0500

    added AdsRepository

---
+++ src/main/java/com/example/HC_SHARNIR_BOT/repository/AdsRepository.java
@@ -0,0 +1,8 @@
+package com.example.HC_SHARNIR_BOT.repository;
+
+import com.example.HC_SHARNIR_BOT.model.Ads;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AdsRepository extends CrudRepository<Ads, Long> {
+
+}
