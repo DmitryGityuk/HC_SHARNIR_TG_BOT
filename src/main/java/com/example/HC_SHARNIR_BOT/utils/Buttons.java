fc432011325eefce67c416d41c61e4afb7ee02a3
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-25 23:41:52 +0500

    added Buttons

---
+++ src/main/java/com/example/HC_SHARNIR_BOT/utils/Buttons.java
@@ -0,0 +1,35 @@
+package com.example.HC_SHARNIR_BOT.utils;
+
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Getter
+public class Buttons {
+    private final String YES = "YES_BUTTON";
+    private final String NO = "NO_BUTTON";
+
+    public InlineKeyboardMarkup getInlineKeyboardMarkup() {
+        InlineKeyboardMarkup inlineMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+        var yesButton = new InlineKeyboardButton();
+        yesButton.setText("Да");
+        yesButton.setCallbackData(YES);
+        var noButton = new InlineKeyboardButton();
+        noButton.setText("Нет");
+        noButton.setCallbackData(NO);
+
+        rowInLine.add(yesButton);
+        rowInLine.add(noButton);
+        rowsInLine.add(rowInLine);
+        inlineMarkup.setKeyboard(rowsInLine);
+
+        return inlineMarkup;
+    }
+}
