cd1733cacaccd1d6ee985f91ecc7ed983e9790b3
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-10-26 23:18:39 +0500

    added methods

--- src/main/java/com/example/HC_SHARNIR_BOT/service/UserService.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/UserService.java
@@ -2,9 +2,19 @@ package com.example.HC_SHARNIR_BOT.service;
 
 import org.telegram.telegrambots.meta.api.objects.Message;
 
+import java.util.List;
+
 
 public interface UserService {
 
     void registerUser(Message msg);
-    }
+
+    List<String> showTop3Assists(Message msg);
+
+    String infoAboutMe(Message msg);
+
+    List<String> showTop3Players(Message msg);
+
+    List<String> showTop3Scorers(Message msg);
+}
 
