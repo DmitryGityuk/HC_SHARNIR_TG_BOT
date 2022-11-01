cb3a48617c2b83ff8e013f3280408c5acd5ec57e
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-11-02 02:31:07 +0500

    changed UserService

--- src/main/java/com/example/HC_SHARNIR_BOT/service/UserService.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/UserService.java
@@ -2,19 +2,19 @@ package com.example.HC_SHARNIR_BOT.service;
 
 import org.telegram.telegrambots.meta.api.objects.Message;
 
-import java.util.List;
-
 
 public interface UserService {
 
     void registerUser(Message msg);
 
-    List<String> showTop3Assists(Message msg);
+    String showTop3Assists(Message msg);
 
     String infoAboutMe(Message msg);
 
-    List<String> showTop3Players(Message msg);
+    String showTop3Players();
 
-    List<String> showTop3Scorers(Message msg);
+    String showTop3Scorers(Message msg);
+
+    String showAllPlayers(Message msg);
 }
 
