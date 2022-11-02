ed2831b2502280b112484d6081ec088c5ad63adf
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-11-02 22:32:16 +0500

    added methods for searching players by parameters

--- src/main/java/com/example/HC_SHARNIR_BOT/repository/UserRepository.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/repository/UserRepository.java
@@ -2,7 +2,19 @@ package com.example.HC_SHARNIR_BOT.repository;
 
 
 import com.example.HC_SHARNIR_BOT.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query(nativeQuery = true, value = "SELECT * FROM players_data_table ORDER BY assists DESC LIMIT 3")
+    List<User> getUserByAssists();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM players_data_table ORDER BY comb DESC LIMIT 3")
+    List<User> getUserByComb();
+
+    @Query(nativeQuery = true, value = "SELECT * FROM players_data_table ORDER BY goals DESC LIMIT 3")
+    List<User> getUserByGoals();
 }
