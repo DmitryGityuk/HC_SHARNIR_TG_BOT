9f559107bd99222af9e300ca0b2a85185ea57dd8
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-11-11 22:40:04 +0500

    added RegisterService

---
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/RegisterService.java
@@ -0,0 +1,9 @@
+package com.example.HC_SHARNIR_BOT.service;
+
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public interface RegisterService {
+
+    void registerPlayerOnGame(Update update);
+
+}
