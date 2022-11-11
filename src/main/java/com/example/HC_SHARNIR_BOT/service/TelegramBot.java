69b1a64f4e1486a84f2f79f2017a4177a9077f33
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-11-11 22:39:28 +0500

    updated TelegramBot added methods, optimize code

--- src/main/java/com/example/HC_SHARNIR_BOT/service/TelegramBot.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/TelegramBot.java
@@ -1,65 +1,72 @@
 package com.example.HC_SHARNIR_BOT.service;
 
 import com.example.HC_SHARNIR_BOT.config.BotConfig;
-import lombok.SneakyThrows;
+import com.example.HC_SHARNIR_BOT.service.impl.MessageServiceImpl;
+import com.example.HC_SHARNIR_BOT.service.impl.RegisterServiceImpl;
+import com.example.HC_SHARNIR_BOT.service.impl.UserServiceImpl;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Slf4j
 @Component
+@EnableScheduling
 public class TelegramBot extends TelegramLongPollingBot {
+    private final UserServiceImpl userServiceimpl;
+    private final MainMenu mainMenu;
+    private final MessageServiceImpl messageService;
+    private final RegisterServiceImpl registerService;
+    private final BotConfig config;
 
-    final BotConfig config;
-
-    static final String HELP_TEXT = "Sorry I can't help you";
-
-    public TelegramBot(BotConfig config) {
+    public TelegramBot(BotConfig config, UserServiceImpl service, MainMenu mainMenu, MessageServiceImpl messageService,
+                       RegisterServiceImpl registerService) {
+        this.userServiceimpl = service;
         this.config = config;
-        List<BotCommand> commandList = new ArrayList<>();
-        commandList.add(new BotCommand("/start", "get a welcome message"));
-        commandList.add(new BotCommand("/mydata", "get info about you"));
-        commandList.add(new BotCommand("/topscorers", "get info top 3 scorers"));
-        commandList.add(new BotCommand("/topassists", "get info top 3 assists"));
-        commandList.add(new BotCommand("/topcomb", "get info top 3 players (goals + assists)"));
-        commandList.add(new BotCommand("/help", "info how to used this bot"));
+        this.mainMenu = mainMenu;
+        this.messageService = messageService;
+        this.registerService = registerService;
         try {
-            this.execute(new SetMyCommands(commandList, new BotCommandScopeDefault(), null));
+            this.execute(new SetMyCommands(mainMenu.getCommandList(), new BotCommandScopeDefault(), null));
         } catch (TelegramApiException ex) {
             log.error("Error setting bot's command list: " + ex.getMessage());
         }
     }
 
     @Override
-    public String getBotToken() {
-        return config.getToken();
-    }
-
-    @SneakyThrows
-    @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText();
             long chatId = update.getMessage().getChatId();
             switch (message) {
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    break;
-                case "/help":
-                    sendMessage(chatId, HELP_TEXT);
-                default:
-                    sendMessage(chatId, "Sorry, command was not recognized");
+                case "/start" -> messageService.sendWelcomeMessage(chatId, update.getMessage().getChat().getFirstName());
+                case "/help", "Помощь" -> messageService.sendHelpMessage(chatId, update.getMessage().getChat().getFirstName());
+                case "/mydata", "Про меня" -> messageService.sendMessage(chatId, userServiceimpl.infoAboutMe(update.getMessage()));
+                case "/topassists", "Голевые пасы" -> messageService.sendMessage(chatId, String.valueOf(userServiceimpl.showTop3Assists(update.getMessage())));
+                case "/topscorers", "Голы" -> messageService.sendMessage(chatId, String.valueOf(userServiceimpl.showTop3Scorers(update.getMessage())));
+                case "/topcomb", "Голы + Пасы" -> messageService.sendMessage(chatId, String.valueOf(userServiceimpl.showTop3Players()));
+                case "/allplayers", "Таблица" -> messageService.sendMessage(chatId, String.valueOf(userServiceimpl.showAllPlayers(update.getMessage())));
+                case "Регистрация" -> userServiceimpl.registerUser(update.getMessage());
+                default -> messageService.sendMessage(chatId, "Очень интересно, но ничего не понятно!");
             }
+        } else if (update.hasCallbackQuery()) {
+            registerService.registerPlayerOnGame(update);
         }
+    }
+
+    @Scheduled(cron = "${cron.schedulerSunday1}")
+    private void registerOnMondayGame() {
+        messageService.sendMessageRegisterOnMondayGame();
+    }
+
+    @Scheduled(cron = "${cron.schedulerWednesday1}")
+    private void registerOnThursdayGame() {
+        messageService.sendMessageRegisterOnThursdayGame();
     }
 
     @Override
@@ -67,20 +74,8 @@ public class TelegramBot extends TelegramLongPollingBot {
         return config.getBotName();
     }
 
-    private void startCommandReceived(long chatId, String name) {
-        String answer = "Hi, " + name + ", nice to meet you";
-        log.info("Replied to user " + name);
-        sendMessage(chatId, answer);
-    }
-
-    private void sendMessage(long chatId, String textToSend) {
-        SendMessage message = new SendMessage();
-        message.setChatId(String.valueOf(chatId));
-        message.setText(textToSend);
-        try {
-            execute(message);
-        } catch (TelegramApiException ex) {
-            log.error("Error " + ex.getMessage());
-        }
+    @Override
+    public String getBotToken() {
+        return config.getToken();
     }
 }
