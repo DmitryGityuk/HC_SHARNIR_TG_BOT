e4017ae0adfe7f509391f8def6adfd4880634daf
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-12-20 00:27:22 +0500

    added new notifications added list notifications added schedulers

--- src/main/java/com/example/HC_SHARNIR_BOT/service/impl/NotificationsServiceImpl.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/impl/NotificationsServiceImpl.java
@@ -2,22 +2,34 @@ package com.example.HC_SHARNIR_BOT.service.impl;
 
 import com.example.HC_SHARNIR_BOT.service.NotificationsService;
 import com.example.HC_SHARNIR_BOT.utils.Emojis;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 @Service
 public class NotificationsServiceImpl implements NotificationsService {
     private final MessageServiceImpl messageService;
     private final String GROUP_ID = "-1001387017494";
+    private List<String> payNotifications;
 
     public NotificationsServiceImpl(MessageServiceImpl messageService) {
         this.messageService = messageService;
     }
 
+    @Scheduled(cron = "${cron.schedulerMonday}")
+    @Scheduled(cron = "${cron.schedulerThursday}")
     @Override
     public void sendNotification() {
+        payNotifications = Arrays.asList("Парни, поактивнее скидываем монету, не стесняемся" + Emojis.MOYAI,
+                "Парни, деньги скидываем, не стесняемся, сумма та же, правила те же " + Emojis.COPYRIGHT);
+        Random rand = new Random();
+        String random = payNotifications.get(rand.nextInt(payNotifications.size()));
         SendMessage msg = new SendMessage();
-        msg.setText("Парни, деньги скидываем, не стесняемся, сумма та же, правила те же " + Emojis.COPYRIGHT);
+        msg.setText(random);
         msg.setChatId(GROUP_ID);
         messageService.sendSimpleMsg(msg);
     }
