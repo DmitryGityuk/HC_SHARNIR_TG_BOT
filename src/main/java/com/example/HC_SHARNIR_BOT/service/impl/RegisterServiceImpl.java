6e5ac13b6a39767f1a7df7a13f10741499baaa31
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-12-20 00:24:54 +0500

    changed method sending listPlayers, edit logic method register on game

--- src/main/java/com/example/HC_SHARNIR_BOT/service/impl/RegisterServiceImpl.java
+++ src/main/java/com/example/HC_SHARNIR_BOT/service/impl/RegisterServiceImpl.java
@@ -1,18 +1,21 @@
 package com.example.HC_SHARNIR_BOT.service.impl;
 
+import com.example.HC_SHARNIR_BOT.repository.AdsRepository;
+import com.example.HC_SHARNIR_BOT.repository.UserRepository;
 import com.example.HC_SHARNIR_BOT.service.RegisterService;
 import com.example.HC_SHARNIR_BOT.utils.Buttons;
-import com.example.HC_SHARNIR_BOT.utils.Emojis;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Service
 @Slf4j
@@ -20,45 +23,111 @@ import java.util.Map;
 @EnableScheduling
 public class RegisterServiceImpl implements RegisterService {
     private final MessageServiceImpl messageService;
+    private final AdsRepository adsRepository;
     private final Buttons buttons;
-    private Map<String, String> players;
     private final String GROUP_ID = "-1001387017494";
+    private Set<String> plus = new HashSet<>();
+    private Set<String> minus = new HashSet<>();
+    private final Long GEN_ID = 1L;
+    private final UserRepository userRepository;
 
-    public RegisterServiceImpl(MessageServiceImpl messageService, Buttons buttons) {
+    public RegisterServiceImpl(MessageServiceImpl messageService, AdsRepository adsRepository, Buttons buttons, UserRepository userRepository) {
         this.messageService = messageService;
+        this.adsRepository = adsRepository;
         this.buttons = buttons;
+        this.userRepository = userRepository;
     }
 
     @Override
     public void registerPlayerOnGame(Update update) {
-        if (update.hasCallbackQuery()) {
-            String callbackData = update.getCallbackQuery().getData();
-            long messageId = update.getCallbackQuery().getMessage().getMessageId();
-            long chatId = update.getCallbackQuery().getMessage().getChatId();
-            if (callbackData.equals(buttons.getYES())) {
-                String txt = "Отлично, добавил тебя в список. \nСписок всех игроков я отправлю сегодня в 20:00 в общий чат " + Emojis.SPEECH_BALLOON;
-                players = new HashMap<>();
-                players.put(update.getCallbackQuery().getFrom().getFirstName(), update.getCallbackQuery().getFrom().getLastName());
-                log.info("добавил игрока " + update.getCallbackQuery().getFrom().getFirstName());
-                messageService.sendEditMessage(txt, chatId, messageId);
-            } else if (callbackData.equals(buttons.getNO())) {
-                String txt = "Ладули, тогда ждём тебя на следующей игре";
-                messageService.sendEditMessage(txt, chatId, messageId);
+        setDay(update, GEN_ID);
+    }
+
+    private void setDay(Update update, long day) {
+        String title = adsRepository.findById(day).get().getMsg() + "\n";
+        String callbackData = update.getCallbackQuery().getData();
+        String fullName = update.getCallbackQuery().getFrom().getLastName() + " " + update.getCallbackQuery().getFrom().getFirstName();
+        if (fullName.equals("Sobolev Den")) {
+            fullName = "Соболев Денис";
+        }
+        if (fullName.equals("Lobastov Kirill")) {
+            fullName = "Лобастов Кирилл";
+        }
+        if (fullName.equals("null Alexey")) {
+            fullName = "Лёха";
+        }
+        if (fullName.equals("null Дмитрий")) {
+            fullName = "Гитюк Дима";
+        }
+        if (fullName.equals("Mezentsev Mark")) {
+            fullName = "Мезенцев Марк";
+        }
+        if (fullName.equals("null Danya Chel")) {
+            fullName = "Лаптев Данил";
+        }
+        if (fullName.equals("Vigandt Artem")) {
+            fullName = "Вигандт Артём";
+        }
+        if (fullName.equals("Gu Ma")) {
+            fullName = "Чистов Никита";
+        }
+        if (fullName.equals("Jorge Jorge")) {
+            fullName = "Кузнецов Гоша";
+        }
+        if (fullName.equals("Podzhiganov Andrey")) {
+            fullName = "Поджиганов Андрей";
+        }
+        if (fullName.equals("null Dmitriy")) {
+            fullName = "Звягинцев Дима";
+        }
+        if (fullName.equals("null Никита")) {
+            fullName = "Нациевский Никита";
+        }
+        long messageId = update.getCallbackQuery().getMessage().getMessageId();
+        long chatId = update.getCallbackQuery().getMessage().getChatId();
+        if (callbackData.equals(buttons.getYES())) {
+            String peoplePlus = "Проголосовал за '+' - " + update.getCallbackQuery().getFrom().getFirstName() + " " + update.getCallbackQuery().getFrom().getUserName() + "\n";
+            plus.add(fullName);
+            if (minus.contains(update.getCallbackQuery().getFrom().getLastName() + " " + update.getCallbackQuery().getFrom().getFirstName())) {
+                minus.remove(update.getCallbackQuery().getFrom().getLastName() + " " + update.getCallbackQuery().getFrom().getFirstName());
+                log.info("remove player from map minus");
             }
+            log.info("добавил игрока в мапу плюсов" + update.getCallbackQuery().getFrom().getFirstName() + " " + update.getCallbackQuery().getFrom().getId());
+
+            messageService.sendEditMessageWithMarkup((title + peoplePlus + textTitle()), chatId, messageId);
+        } else if (callbackData.equals(buttons.getNO())) {
+            String peopleMinus = "Проголосовал за '-' - " + update.getCallbackQuery().getFrom().getFirstName() + " " + update.getCallbackQuery().getFrom().getUserName() + "\n";
+            minus.add(fullName);
+            if (plus.contains(update.getCallbackQuery().getFrom().getLastName() + " " + update.getCallbackQuery().getFrom().getFirstName())) {
+                plus.remove(update.getCallbackQuery().getFrom().getLastName() + " " + update.getCallbackQuery().getFrom().getFirstName());
+                log.info("remove player from map plus");
+            }
+            log.info("добавил игрока в мапу минусов" + update.getCallbackQuery().getFrom().getFirstName());
+            messageService.sendEditMessageWithMarkup((title + peopleMinus + textTitle()), chatId, messageId);
         }
     }
 
     public void sendListPlayers() {
-        SendMessage msg = new SendMessage();
-        msg.setText("Список игроков на завтра:\n" + playersOnGame(getPlayers()));
-        msg.setChatId(GROUP_ID);
-        messageService.sendSimpleMsg(msg);
+        SendMessage list = new SendMessage();
+        list.setText(playersOnGame());
+        list.setChatId(GROUP_ID);
+        messageService.sendSimpleMsg(list);
     }
 
-    public <K, V> String playersOnGame(Map<K, V> players) {
-        String firstName = players.keySet().toString().replaceAll("[\\p{Ps}\\p{Pe}]", "");
-        String secondName = players.values().toString().replaceAll("[\\p{Ps}\\p{Pe}]", "");
-        String fullName = firstName + " " + secondName;
-        return fullName;
+    public String playersOnGame() {
+        String name;
+        List<String> p = List.copyOf(plus);
+        List<String> players = new ArrayList<>();
+        for (String pl : p) {
+            name = pl + "\n";
+            players.add(name);
+        }
+        return String.valueOf(players).replaceAll("\\p{P}", "");
+    }
+
+    public String textTitle() {
+        String txt = "+ " + plus.size() + ":\n" + getPlus().toString().replaceAll("[^\\p{L}\\p{N}]+", " ") + "\n " +
+                "- " + minus.size() + ":\n" + getMinus().toString().replaceAll("[^\\p{L}\\p{N}]+", " ");
+        return txt;
     }
 }
